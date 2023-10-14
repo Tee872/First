911a1e7030cac8862cb087978c81a5021d35ebb4
Natee Thongsuk <teeandnarita@Tees-MacBook-Air.local>
2023-10-14 15:07:00 +0700

    First

---
+++ TEstgib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TEstgib
+//
+//  Created by Natee Thongsuk on 10/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+// i am trying to push git
+}
+
